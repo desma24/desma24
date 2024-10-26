15c9fd8b3119c330405da2db3715c7605f23253e
desma24 <desma8778@gmail.com>
2024-10-26 13:23:52 +0700

    Create pertemuan10.cpp

---
+++ pertemuan10.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Program untuk mencetak angka 1 sampai 5
+    for (int i = 1; i <= 5; i++) {
+        cout << i << endl;
+    }
+    return 0;
+}
